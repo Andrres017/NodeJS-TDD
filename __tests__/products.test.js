1bc2aaa849e35eebbc40471fa99de2c6a5fe2d0d
Andres felipe (daratot) <Andrres017@gmail.com>
2023-06-02 23:46:34 -0500

    Pruebas unitarias

--- __tests__/products.test.js
+++ __tests__/products.test.js
@@ -1,24 +1,30 @@
 const request = require('supertest')
 
 const { app } = require('../server')
-
 const { Builder } = require('../builders/product-builder.js')
-// aqui colocaremos las pruebas...
+const { store } = require('../services/product-service')
+
+jest.mock('../services/product-service.js')
+
+beforeEach(() => {
+  store.mockReset()
+})
 
 describe('POST /products', () => {
   test('should store a new product', async () => {
+    // code...
+  })
+
+  test('should execute store function', async () => {
     const product = Builder.product()
 
-    const response = await request(app)
+    await request(app)
       .post('/products')
       .send(product)
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(201)
 
-    expect(response.body).toEqual({
-      ...product,
-      _id: 'abc',
-    })
+    expect(store).toHaveBeenCalledWith(product)
   })
 })
