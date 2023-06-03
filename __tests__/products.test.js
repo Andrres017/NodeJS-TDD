c2b44dd6b1f45d4409dffcaf33381ba7c1ea2e22
Andres felipe (daratot) <Andrres017@gmail.com>
2023-06-02 23:17:19 -0500

    mi primer TDD

---
+++ __tests__/products.test.js
@@ -0,0 +1,24 @@
+const request = require('supertest')
+
+const { app } = require('../server')
+
+const { Builder } = require('../builders/product-builder.js')
+// aqui colocaremos las pruebas...
+
+describe('POST /products', () => {
+  test('should store a new product', async () => {
+    const product = Builder.product()
+
+    const response = await request(app)
+      .post('/products')
+      .send(product)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(201)
+
+    expect(response.body).toEqual({
+      ...product,
+      _id: 'abc',
+    })
+  })
+})
